bd5322aa71679254d33bc94616566b5be39a144a
vinod102003 <123479516+vinod102003@users.noreply.github.com>
2023-07-08 20:19:37 +0530

    Create q4.java

---
+++ ppt_4/q4.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ArrayPartition {
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 4, 3, 2};
+        int maxSum = arrayPairSum(nums);
+        System.out.println("Maximized sum: " + maxSum);
+    }
+}
