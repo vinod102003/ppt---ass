f8ef45fd9dec50f032d5c6038868a6e5849fddee
vinod102003 <123479516+vinod102003@users.noreply.github.com>
2023-07-08 20:07:33 +0530

    Create q2.java

---
+++ pp_3/q2.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        
+        if (n < 4) {
+            return result;
+        }
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                
+                int left = j + 1;
+                int right = n - 1;
+                
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+                        
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        List<List<Integer>> quadruplets = fourSum(nums, target);
+        
+        System.out.println("Unique quadruplets that sum up to the target:");
+        for (List<Integer> quadruplet : quadruplets) {
+            System.out.println(quadruplet);
+        }
+    }
+}
